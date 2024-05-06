6d1f98ecec9033724ee66487b402ffbacd63d4a6
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-06 18:10:18 +0545

    final commit for eye icon

--- js/scriptlogin.js
+++ js/scriptlogin.js
@@ -1,11 +1,10 @@
 const eyeIcon = document.getElementById("eyee");
 const inputBox = document.getElementById("password");
-
 eyeIcon.addEventListener("click", () => {
   console.log("eyesss");
   if (inputBox.type === "password") {
     inputBox.type = "text";
-    eyeIcon.classlist.replace("fa-eye-slash", "fa-eye");
+    eyeIcon.classList.replace("fa-eye-slash", "fa-eye");
   } else {
     inputBox.type = "password";
     eyeIcon.classList.replace("fa-eye", "fa-eye-slash");
