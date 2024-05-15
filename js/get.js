c272dc10c8e996d6273df889ecfea34abf6e1baf
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-15 23:53:06 +0545

    db added

--- js/get.js
+++ js/get.js
@@ -1,6 +1,6 @@
 const myfunction = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   const data = await response.json();
-  console.log(data);
+  console.log(data.title);
 };
 myfunction();
