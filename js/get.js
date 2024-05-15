a896a302a7b30fb9fb9a3111f4709129441f86d2
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-15 23:38:57 +0545

    async

--- js/get.js
+++ js/get.js
@@ -1,2 +1,6 @@
-const response = fetch("http://lms.sachetsubedi001.com.np");
-console.log(response);
+const myfunction = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await response.json();
+  console.log(data);
+};
+myfunction();
