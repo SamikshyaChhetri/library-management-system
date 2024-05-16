e451db3fecfce477dbd4feb35cf7c7a7cd16ebdc
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-16 18:25:13 +0545

    space removed

--- js/get.js
+++ js/get.js
@@ -1,6 +1,25 @@
-const myfunction = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+// const myfunction = async () => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   const data = await response.json();
+//   console.log(data.title);
+// };
+// myfunction();
+const func = async () => {
+  const response = await fetch(
+    "https://lms.sachetsubedi001.com.np/api/auth/register",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "Neha",
+        email: "nehakakkar@gmail.com",
+        password: "hello",
+      }),
+    }
+  );
   const data = await response.json();
-  console.log(data.title);
+  console.log(data);
 };
-myfunction();
+func();
