2414b23b2df0f13e91f43cf692c0e5d7c7de4a8a
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-18 10:48:02 +0545

    setInterval added to js

--- js/dashboard.js
+++ js/dashboard.js
@@ -21,27 +21,28 @@ week = [
   "Friday",
   "Saturday",
 ];
-const element = document.getElementById("time");
-let curr_date = new Date();
-let month = curr_date.getMonth();
-let date = curr_date.getDate();
-let year = curr_date.getFullYear();
-let day = curr_date.getDay();
-let hours = curr_date.getHours();
-let min = curr_date.getMinutes();
-console.log(arr[month]);
-console.log(date);
-console.log(year);
-console.log(week[day - 1]);
-element.innerHTML =
-  arr[month] +
-  " " +
-  date +
-  ", " +
-  year +
-  " " +
-  week[day - 1] +
-  " " +
-  hours +
-  ":" +
-  min;
+setInterval(() => {
+  const element = document.getElementById("time");
+  let curr_date = new Date();
+  let month = curr_date.getMonth();
+  let date = curr_date.getDate();
+  let year = curr_date.getFullYear();
+  let day = curr_date.getDay();
+  let hours = curr_date.getHours();
+  let min = curr_date.getMinutes();
+  let sec = curr_date.getSeconds();
+  element.innerHTML =
+    arr[month] +
+    " " +
+    date +
+    ", " +
+    year +
+    " " +
+    week[day - 1] +
+    " " +
+    hours +
+    ":" +
+    min +
+    ":" +
+    sec;
+}, 100);
