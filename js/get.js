049a68ff83e675dea95a5123091f60ca2efdcdc8
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-19 22:56:39 +0545

    padding removed

--- js/get.js
+++ js/get.js
@@ -2,7 +2,6 @@
 //   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
 //   const data = await response.json();
 //   console.log(data.title);
-// };
 const func = async () => {
   const response = await fetch(
     "https://lms.sachetsubedi001.com.np/api/auth/register",
