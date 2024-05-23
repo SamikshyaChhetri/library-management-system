4582bb0e374a008d309f14e8cf6c1ef5096bba69
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-23 23:26:26 +0545

    api fetch

--- js/regStyle.js
+++ js/regStyle.js
@@ -4,25 +4,38 @@ submit_button.addEventListener("click", async (e) => {
   e.preventDefault();
   submit_msg.innerHTML = "Please wait...";
   const emailName = document.getElementById("email").value;
-  const pw = document.getElementById("password").value;
+  const pw = document.getElementById("pw").value;
+  const confirm_pw = document.getElementById("confirm").value;
   const username = document.getElementById("name").value;
-  const data_pass = {
-    email: emailName,
-    password: pw,
-    name: username,
-  };
-  const response = await fetch(
-    "https://lms.sachetsubedi001.com.np/api/auth/register",
-    {
-      method: "post",
-      body: JSON.stringify(data_pass),
-      headers: {
-        "Content-Type": "application/json",
-      },
+  if (pw == confirm_pw) {
+    const data_pass = {
+      email: emailName,
+      password: pw,
+      name: username,
+    };
+    const response = await fetch(
+      "https://lms.sachetsubedi001.com.np/api/auth/register",
+      {
+        method: "post",
+        body: JSON.stringify(data_pass),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    console.log(response.status);
+    submit_msg.innerHTML = "Register";
+
+    if (response.status == "200") {
+      localStorage.setItem("token", data.data.token);
+      console.log(data);
+      window.location.href = "../pages/dashboard.html";
+    } else {
+      alert(data.message);
     }
-  );
-  const data = await response.json();
-  console.log(data);
-  console.log(response.status);
-  submit_msg.innerHTML = "Login";
+  } else {
+    alert("confirmed password didnot match");
+  }
 });
