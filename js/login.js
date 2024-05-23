13f62fe94623055e5e6ea6e6914f85622a148e7c
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-23 12:57:11 +0545

    email & pw fetched

--- js/login.js
+++ js/login.js
@@ -13,30 +13,32 @@ eyeIcon.addEventListener("click", () => {
 // const loc = window.location.href;
 // console.log(loc);
 
+// submit_msg.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   let mail = "samikshyabchhetri@gmail.com";
+//   let pw = "neheart";
+//   const email_name = document.getElementById("email").value;
+//   const pww = document.getElementById("password").value;
+//   if (mail === email_name && pw === pww) {
+//     console.log("correct");
+//     window.location.href = "../pages/dashboard.html";
+//   } else {
+//     window.alert("Invalid email or password");
+//   }
+// });
 const submit_msg = document.getElementById("submit");
-submit_msg.addEventListener("click", (e) => {
+submit_msg.addEventListener("click", async (e) => {
   e.preventDefault();
-  let mail = "samikshyabchhetri@gmail.com";
-  let pw = "neheart";
-  const email_name = document.getElementById("email").value;
-  const pww = document.getElementById("password").value;
-  if (mail === email_name && pw === pww) {
-    console.log("correct");
-    window.location.href = "../pages/dashboard.html";
-  } else {
-    window.alert("Invalid email or password");
-  }
-});
-const data_pass = {
-  email: "alxisamikshya@gmail.com",
-  password: "alxi",
-};
-async function fetchSignIN() {
+  const emailName = document.getElementById("email").value;
+  const pw = document.getElementById("password").value;
+  const data_pass = {
+    email: emailName,
+    password: pw,
+  };
   const response = await fetch(
     "https://lms.sachetsubedi001.com.np/api/auth/logIn",
     {
       method: "post",
-      body: "data",
       body: JSON.stringify(data_pass),
       headers: {
         "Content-Type": "application/json",
@@ -45,5 +47,14 @@ async function fetchSignIN() {
   );
   const data = await response.json();
   console.log(data);
-}
-fetchSignIN();
+  console.log(response.status);
+  if (response.status == "200") {
+    window.location.href = "../pages/dashboard.html";
+  } else if (response.status == "401") {
+    alert("Invalid  password");
+  } else if (response.status == "404") {
+    alert("User not found");
+  } else {
+    alert("Server error");
+  }
+});
