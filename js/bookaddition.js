07da1dd29fa2b470eed97f2860adb2cccfb00b5e
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-30 23:24:08 +0545

    stored input value

--- js/bookaddition.js
+++ js/bookaddition.js
@@ -1,1 +1,13 @@
 const submitBooks = document.getElementById("btn");
+submitBooks.addEventListener("click", () => {
+  const bookname = document.getElementById("bookTitle").value;
+  const authorName = document.getElementById("author").value;
+  const bookPage = document.getElementById("page").value;
+  const bookPrice = document.getElementById("price").value;
+  const dataPass = {
+    title: bookname,
+    author: authorName,
+    pages: bookPage,
+    price: bookPrice,
+  };
+});
