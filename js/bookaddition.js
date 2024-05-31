00adec500fd6dee5283da05ade4a1ad50b28bc76
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-31 21:47:50 +0545

    modified toast msg

--- js/bookaddition.js
+++ js/bookaddition.js
@@ -34,32 +34,18 @@ submitBooks.addEventListener("click", async (e) => {
   if (!bookname) {
     const bkname = document.getElementById("bookTitle");
     bkname.style.border = "1.2px solid red";
-    // removeBorder("1.2px solid blueviolet");
-    // toast("Enter the Title", "red");
-    // removetoast();
   }
   if (!authorName) {
     const authName = document.getElementById("author");
     authName.style.border = "1.2px solid red";
-    // removeBorder("1.2px solid blueviolet");
-    // toast("Enter Author name", "red");
-    // removetoast();
   }
   if (!bookPage) {
     const bkpage = document.getElementById("page");
     bkpage.style.border = "1.2px solid red";
-    // removeBorder("1.2px solid blueviolet");
-
-    // toast("Enter bookpage", "red");
-    // removetoast();
   }
   if (!bookPrice) {
     const bkprice = document.getElementById("price");
     bkprice.style.border = "1.2px solid red";
-    // removeBorder("1.2px solid blueviolet");
-
-    // toast("Enter bookprice");
-    // removetoast();
   } else {
     toast("Book added successfully", "green");
     removetoast();
