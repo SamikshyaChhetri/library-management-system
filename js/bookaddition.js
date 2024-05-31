17be94f15b54e39a650c7d9cc32163e52c3660b0
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-31 19:20:07 +0545

    back to normal border

--- js/bookaddition.js
+++ js/bookaddition.js
@@ -33,23 +33,29 @@ submitBooks.addEventListener("click", async (e) => {
   } else if (!bookname) {
     const bkname = document.getElementById("bookTitle");
     bkname.style.border = "1.2px solid red";
+    removeBorder("1.2px solid blueviolet");
 
     // toast("Enter the Title", "red");
     // removetoast();
   } else if (!authorName) {
     const authName = document.getElementById("author");
     authName.style.border = "1.2px solid red";
+    removeBorder("1.2px solid blueviolet");
 
     // toast("Enter Author name", "red");
     // removetoast();
   } else if (!bookPage) {
     const bkpage = document.getElementById("page");
     bkpage.style.border = "1.2px solid red";
+    removeBorder("1.2px solid blueviolet");
+
     // toast("Enter bookpage", "red");
     // removetoast();
   } else if (!bookPrice) {
     const bkprice = document.getElementById("price");
     bkprice.style.border = "1.2px solid red";
+    removeBorder("1.2px solid blueviolet");
+
     // toast("Enter bookprice");
     // removetoast();
   } else {
