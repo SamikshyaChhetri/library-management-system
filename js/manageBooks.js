1d8e7901883718d076c610de9da13cf4ad946647
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-12 18:20:36 +0545

    return button function implemented

--- js/manageBooks.js
+++ js/manageBooks.js
@@ -6,20 +6,45 @@ const reservedBooks = async () => {
       useridfromlocalstorage
   );
   const data = await response.json();
-  console.log(data.data);
+  //   console.log(data.data);
   const displayReservedBooks = document.getElementById("booksContainer");
   data.data.forEach((element) => {
-    console.log(element);
+    // console.log(element);
     displayReservedBooks.innerHTML += `
           <div class="book">
         <div class="manageBooks">
           <div class="book1"> ${element.book.title}</div>
-          <div class="reserveDate">Date of reservation</div>
+          <div class="reserveDate">${element.createdAt}</div>
         </div>
-        <div class="return">Return</div>
+        <div class="return" data-id=${element.id}>Return</div>
       </div>
           `;
   });
 };
 reservedBooks();
-//hawaaaaaaaaaaa sabai ko name tassera aaira xa ani aautai jsto dekheko
+
+//Returning book part
+
+const returnBookButton = document.querySelectorAll("return");
+returnBookButton.forEach((e) => {
+  e.addEventListener("click", () => {
+    const returnBookId = e.getAttribute("data-id");
+    localStorage.setItem("returnBookID", returnBookId);
+    const returningBookIDfromlocalstorage=localStorage.getItem("returnBookID")
+    console.log(returnBookId);
+    const data_pass={
+        returnBookID:returningBookIDfromlocalstorage
+    }
+    const response=await fetch("https://lms.sachetsubedi001.com.np/api/reservations/" + returnBookId,
+    {
+        method: "post",
+      body: JSON.stringify(data_pass),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    const data= await response.json();
+    console.log(data)
+  });
+});
+returnBookButton();
