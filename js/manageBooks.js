50f6b132e5f08c6dfd6917db079e10740e7d26b6
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-12 17:04:56 +0545

    styled reserved books of user

--- js/manageBooks.js
+++ js/manageBooks.js
@@ -7,13 +7,19 @@ const reservedBooks = async () => {
   );
   const data = await response.json();
   console.log(data.data);
-  const displayReservedBooks = document.getElementById("book");
+  const displayReservedBooks = document.getElementById("booksContainer");
   data.data.forEach((element) => {
+    console.log(element);
     displayReservedBooks.innerHTML += `
-          <tr>
-            <td>${element.book.author}</td>
-          </tr>
+          <div class="book">
+        <div class="manageBooks">
+          <div class="book1"> ${element.book.title}</div>
+          <div class="reserveDate">Date of reservation</div>
+        </div>
+        <div class="return">Return</div>
+      </div>
           `;
   });
 };
 reservedBooks();
+//hawaaaaaaaaaaa sabai ko name tassera aaira xa ani aautai jsto dekheko
