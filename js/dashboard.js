5ef48e7f8420f64d2c8047c5ede1541fd2a4ac93
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-15 20:11:12 +0545

    userlist added

--- js/dashboard.js
+++ js/dashboard.js
@@ -44,56 +44,6 @@ setInterval(() => {
     min;
 }, 1000);
 
-// const people = [
-//   {
-//     userId: 1,
-//     userName: "Alice Smith",
-//     bookIssued: "JavaScript: The Good Parts",
-//     department: "Computer Science",
-//   },
-//   {
-//     userId: 2,
-//     userName: "Bob Johnson",
-//     bookIssued: "Introduction to Algorithms",
-//     department: "Engineering",
-//   },
-//   {
-//     userId: 3,
-//     userName: "Charlie Brown",
-//     bookIssued: "Design Patterns",
-//     department: "Software Development",
-//   },
-//   {
-//     userId: 4,
-//     userName: "Diana Prince",
-//     bookIssued: "Artificial Intelligence",
-//     department: "AI Research",
-//   },
-//   {
-//     userId: 5,
-//     userName: "Eve Davis",
-//     bookIssued: "Data Science for Business",
-//     department: "Data Science",
-//   },
-//   {
-//     userId: 10,
-//     userName: "John Smith",
-//     bookIssued: "Introduction to Machine Learning",
-//     department: "Computer Science",
-//   },
-// ];
-// const display = document.getElementById("tableBody");
-// people.forEach((person) => {
-//   display.innerHTML += `
-//         <tr>
-//           <td>${person.userId}</td>
-//           <td>${person.userName}</td>
-//           <td>${person.bookIssued}</td>
-//           <td>${person.department}</td>
-//         </tr>
-//       `;
-// });
-
 const getUserData = async () => {
   const response = await fetch("https://lms.sachetsubedi001.com.np/api/users");
   const displayUserList = document.getElementById("tableBody");
@@ -104,7 +54,7 @@ const getUserData = async () => {
   sliceUserData.forEach((user) => {
     displayUserList.innerHTML += `
     <tr>
-        <td>${user.id}</td>
+
         <td>${user.name}</td>
         <td>${user.email}</td>
       </tr>
