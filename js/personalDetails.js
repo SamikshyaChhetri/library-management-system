4a1743f6071e664ea758e4db17c4508ec0c7fb5f
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-17 21:14:55 +0545

    Linked toast.js

--- js/personalDetails.js
+++ js/personalDetails.js
@@ -2,29 +2,30 @@ document.getElementById("saveChanges").addEventListener("click", async (e) => {
   e.preventDefault();
   const fullName = document.getElementById("name").value;
   const phoneNumber = document.getElementById("phoneNum").value;
-  const streetName = document.getElementById("street").value;
+  const emailName = document.getElementById("email").value;
   const apartmentName = document.getElementById("apartment").value;
   const cityName = document.getElementById("city").value;
   const stateName = document.getElementById("state").value;
   const zipCode = document.getElementById("zipcode").value;
   const countryName = document.getElementById("countryId").value;
   const useridfromlocalstorage = localStorage.getItem("userid");
-  console.log(useridfromlocalstorage);
+
   const dataPass = {
     id: useridfromlocalstorage,
     name: fullName,
     phone: phoneNumber,
     apartment: apartmentName,
+    email: emailName,
     city: cityName,
-    street: streetName,
     state: stateName,
     zip: zipCode,
     country: countryName,
   };
+  console.log(dataPass);
   const response = await fetch(
-    "https://lms.sachetsubedi001.com.np/api/users/",
+    "https://lms.sachetsubedi001.com.np/api/users/update",
     {
-      method: "PATCH",
+      method: "POST",
       body: JSON.stringify(dataPass),
       headers: {
         "Content-Type": "application/json",
