3183a18f735805e34051b55b1fd1c35ec8e1939d
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-17 19:18:45 +0545

    fetched api

--- js/personalDetails.js
+++ js/personalDetails.js
@@ -1,13 +1,13 @@
-const fullName = document.getElementById("name").value;
-const phoneNumber = document.getElementById("phoneNum").value;
-const streetName = document.getElementById("street").value;
-const apartmentName = document.getElementById("apartment").value;
-const cityName = document.getElementById("city").value;
-const stateName = document.getElementById("state").value;
-const zipCode = document.getElementById("zipcode").value;
-const countryName = document.getElementById("countryId").value;
 document.getElementById("saveChanges").addEventListener("click", async (e) => {
   e.preventDefault();
+  const fullName = document.getElementById("name").value;
+  const phoneNumber = document.getElementById("phoneNum").value;
+  const streetName = document.getElementById("street").value;
+  const apartmentName = document.getElementById("apartment").value;
+  const cityName = document.getElementById("city").value;
+  const stateName = document.getElementById("state").value;
+  const zipCode = document.getElementById("zipcode").value;
+  const countryName = document.getElementById("countryId").value;
   const dataPass = {
     name: fullName,
     phone: phoneNumber,
@@ -17,4 +17,16 @@ document.getElementById("saveChanges").addEventListener("click", async (e) => {
     zip: zipCode,
     countrt: countryName,
   };
+  const response = await fetch(
+    "https://lms.sachetsubedi001.com.np/api/users/",
+    {
+      method: "patch",
+      body: JSON.stringify(dataPass),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data);
 });
