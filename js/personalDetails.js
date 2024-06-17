17513b48e7d29b6ffa77933193aaa1c3b9451120
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-17 18:29:39 +0545

    Grabbed all the input value

---
+++ js/personalDetails.js
@@ -0,0 +1,7 @@
+const fullName = document.getElementById("name").value;
+const phoneNumber = document.getElementById("phoneNum").value;
+const streetName = document.getElementById("street").value;
+const apartmentName = document.getElementById("apartment").value;
+const cityName = document.getElementById("city").value;
+const stateName = document.getElementById("state").value;
+const zipCode = document.getElementById("zipcode").value;
