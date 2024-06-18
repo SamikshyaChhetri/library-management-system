3765d509563f78c6208b4b781f2482da82a4a3ba
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-18 18:21:18 +0545

    issue solved #21

--- js/personalDetails.js
+++ js/personalDetails.js
@@ -15,17 +15,19 @@ const fetchData = async () => {
   document.getElementById("countryId").value = data.data.country;
 };
 fetchData();
-const fullName = document.getElementById("name").value;
-const phoneNumber = document.getElementById("phoneNum").value;
-const emailName = document.getElementById("email").value;
-const apartmentName = document.getElementById("apartment").value;
-const cityName = document.getElementById("city").value;
-const stateName = document.getElementById("state").value;
-const zipCode = document.getElementById("zipcode").value;
-const countryName = document.getElementById("countryId").value;
 const saveChanges = document.getElementById("saveChanges");
+
 saveChanges.addEventListener("click", async (e) => {
   e.preventDefault();
+  const fullName = document.getElementById("name").value;
+  const phoneNumber = document.getElementById("phoneNum").value;
+  const emailName = document.getElementById("email").value;
+  const apartmentName = document.getElementById("apartment").value;
+  const cityName = document.getElementById("city").value;
+  const stateName = document.getElementById("state").value;
+  const zipCode = document.getElementById("zipcode").value;
+  const countryName = document.getElementById("countryId").value;
+
   saveChanges.innerHTML = "Updating...";
   const dataPass = {
     id: useridfromlocalstorage,
