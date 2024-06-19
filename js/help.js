d849ab694e1343c4ad1e3f90b8087b53d1d5882b
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-19 16:55:45 +0545

    rotated icon

--- js/help.js
+++ js/help.js
@@ -1,44 +1,63 @@
-document.getElementById("icon1").addEventListener("click", () => {
+const icon1 = document.getElementById("icon1");
+icon1.addEventListener("click", () => {
   const displayText = document.getElementById("loginRegister");
   const wrapdisplay = document.getElementById("wrapAll");
   // Check current display style
   if (displayText.style.display == "none") {
     // If currently hidden, show the element
     displayText.style.display = "flex";
+    icon1.style.transform = "rotate(180deg)";
+    icon1.style.transition = "0.5s";
+
     wrapdisplay.style.transition = "0.3s ease-in-out";
     wrapdisplay.style.boxShadow =
       "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px";
   } else {
     // If currently shown, hide the element
     displayText.style.display = "none";
+    icon1.style.transform = "rotate(0deg)";
+    icon1.style.transition = "0.5s";
+
     wrapdisplay.style.boxShadow = "none";
     wrapdisplay.style.transition = "0.3s ease-in-out";
   }
 });
-document.getElementById("icon2").addEventListener("click", () => {
+const icon2 = document.getElementById("icon2");
+icon2.addEventListener("click", () => {
   const displayText = document.getElementById("displayafterLogin");
   const wrapdisplay = document.getElementById("afterLogin");
   // Check current display style
   if (displayText.style.display == "none") {
     // If currently hidden, show the element
+    icon2.style.transform = "rotate(180deg)";
+
     displayText.style.display = "flex";
     wrapdisplay.style.transition = "0.5s ease-in-out";
+    icon2.style.transition = "0.5s";
+
     wrapdisplay.style.boxShadow =
       "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px";
   } else {
     // If currently shown, hide the element
     displayText.style.display = "none";
     wrapdisplay.style.boxShadow = "none";
+    icon2.style.transform = "rotate(0deg)";
+    icon2.style.transition = "0.5s";
+
     wrapdisplay.style.transition = "0.3s ease-in-out";
   }
 });
-document.getElementById("icon3").addEventListener("click", () => {
+const icon3 = document.getElementById("icon3");
+icon3.addEventListener("click", () => {
   const displayText = document.getElementById("profile");
   const wrapdisplay = document.getElementById("wrapprofile");
   // Check current display style
   if (displayText.style.display == "none") {
     // If currently hidden, show the element
     displayText.style.display = "flex";
+    icon3.style.transform = "rotate(180deg)";
+    icon3.style.transition = "0.5s";
+
     wrapdisplay.style.transition = "0.3s ease-in-out";
     wrapdisplay.style.boxShadow =
       "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px";
@@ -47,5 +66,7 @@ document.getElementById("icon3").addEventListener("click", () => {
     displayText.style.display = "none";
     wrapdisplay.style.boxShadow = "none";
     wrapdisplay.style.transition = "0.3s ease-in-out";
+    icon3.style.transform = "rotate(0deg)";
+    icon3.style.transition = "0.5s";
   }
 });
